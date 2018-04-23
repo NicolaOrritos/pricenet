4c5e637a34f02d336cfdf2555ec6a53f49b76f3c
Nicola Orritos <nicola.orritos@gmail.com>
2018-04-24 00:54:30 +0200

    Trying RandomForestRegressor for now

--- sandbox.py
+++ sandbox.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 import requests
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+import numpy as np
 
 
 def GET(url):
@@ -19,16 +19,16 @@ def get_data(url):
     return raw['Data']
 
 
-def prices(data):
+def get_prices(data):
     return [item['close'] for item in data]
 
 
-def volumes(data):
+def get_volumes(data):
     return [item['volumeto'] - item['volumefrom'] for item in data]
 
 
 def group(data, step=4):
-    return [data[a:(a + step - 1)] for a in xrange(0, len(data), step)]
+    return [data[a:(a + step)] for a in range(0, len(data), step)]
 
 
 def build_inputs(data, inputs_count=3, outputs_count=1):
@@ -62,99 +62,62 @@ def baseline_model():
 
     return model
 
-estimator = KerasRegressor(build_fn=baseline_model)
-# estimator.fit()
+
+# Get data:
+curr = 'BTC'
+fiat = 'USD'
+samples = 12 * 80
+exchange = 'CCCAGG'
+
+url = ('https://min-api.cryptocompare.com/data/histohour?'
+    +  'fsym={0}&tsym={1}'
+    +  '&limit={2}'
+    +  '&e={3}'
+    +  '&aggregate=1')
+
+url = url.format(curr, fiat, samples, exchange)
+
+data = get_data(url)
+prices = get_prices(data)
+volumes = get_volumes(data)
+
+grouped_prices  = group(prices, 4)
+grouped_volumes = group(volumes, 4)
+
+# Remove 4th item from each group for the prices
+# and discard it for the volumes.
+# Keep the 4th price of each group as "y".
+three_items_prices  = [group[0:3] for group in grouped_prices if len(group) == 4]
+three_items_volumes = [group[0:3] for group in grouped_volumes if len(group) == 4]
+target_prices       = [group[3] for group in grouped_prices if len(group) == 4]
+
+# Put the three prices and volumes together:
+prices_and_volumes = [prices + volumes for prices, volumes in zip(three_items_prices, three_items_volumes)]
 
 
+# Let's obtain "X" and "y" sets:
+X_train, X_test, y_train, y_test = train_test_split(prices_and_volumes, target_prices, test_size=0.3)
 
 
+X_train = np.array(X_train)
+X_test  = np.array(X_test)
+y_train = np.array(y_train)
+y_test  = np.array(y_test)
 
 
+clf = RandomForestRegressor()
+
+clf.fit(X_train, y_train)
+
+# clf.score(X_test, y_test)
 
 
-# const network = new syn.Architect.Perceptron(6, 12, 1)
-#
-# const exchange = 'CCCAGG'
-# const curr = 'BTC'
-# const fiat = 'USD'
-# const samples = 12 * 80
-#
-# const url = `https://min-api.cryptocompare.com/data/histohour?`
-#           + `fsym=${curr}&tsym=${fiat}`
-#           + `&limit=${samples}`
-#           + `&e=${exchange}`
-#           + '&aggregate=1'
-#
-# console.log(`Calling URL "${url}"...`)
-#
-# let volumes
-#
-# getData(url)
-# .then( data =>
-# {
-#     // Save volume triplets to add them later:
-#     return new Promise( (resolve, reject) =>
-#     {
-#         buildVolumeData(data)
-#         .then( data => volumes = data )
-#         .then(   () => resolve(data) )
-#         .catch( err => reject(err) )
-#     })
-# })
-# .then( data => pricesData(data) )
-# .then( data => buildData(data) )
-# .then( data => normalize(data) )
-# .then( data => addVolumes(data, volumes) )
-# .then( data => stringify(data) )
-# .then( trainData =>
-# {
-#     const trainer = new syn.Trainer(network)
-#
-#     const testSamples = 24
-#
-#     // Get last one to be test-data
-#     const testData = trainData.slice(-testSamples)
-#     trainData = trainData.slice(0, -testSamples)
-#
-#     console.log(`Training with ${trainData.length} samples...`)
-#
-#     const options =
-#     {
-#         iterations: 200000,
-#
-#         log: 10000,
-#
-#         shuffle: true,
-#         rate: 0.2,
-#         error: 0.005
-#     }
-#
-#     const result = trainer.train(trainData, options)
-#
-#     console.log(`Training results: ${JSON.stringify(result)}`)
-#
-#     return Promise.resolve(testData)
-# })
-# .then( testData =>
-# {
-#     console.log(`Testing ${testData.length} samples...`)
-#
-#     const outputs = []
-#     let error = 0
-#
-#     for (let item of testData)
-#     {
-#         const output = network.activate(item.input)
-#
-#         outputs.push(output)
-#
-#         error += Math.pow(item.output - output, 2)
-#     }
-#
-#     error = error / testData.length
-#
-#     /* console.log(`Predicted: ${utils.inspect(outputs)}`)
-#     console.log(`Actual:    ${utils.inspect(testData)}`) */
-#     console.log(`Error:     ${error} [MSE]`)
-# })
-# .catch( err => console.log(err) )
+predicted = clf.predict(X_test[0].reshape(-1, 6))
+actual = y_test[0]
+
+
+difference = (actual/predicted - 1) * 100
+
+print('Predicted:', predicted[0])
+print('Actual:   ', actual)
+print('Error:    {}%'.format(round(difference[0], 2)))
