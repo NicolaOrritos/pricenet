1292a7a88787086a2d2ab073cb7ccde0d6a50356
Nicola Orritos <nicola.orritos@gmail.com>
2018-05-04 17:17:29 +0200

    Improved the code and played with an MSE algorithm for validating the model

--- keras_approach.py
+++ keras_approach.py
@@ -11,7 +11,6 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.callbacks import TensorBoard
 from keras.wrappers.scikit_learn import KerasRegressor
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import KFold
@@ -52,6 +51,24 @@ def group(data, step=4):
 
     return data
 
+def scale(data_frame):
+    data_frame -= data_frame.min()
+    data_frame /= data_frame.max()
+
+    return data_frame
+
+def validate_model(model, test_data, test_targets):
+    predictions = []
+    for data in test_data:
+        predictions.append(model.predict(data.reshape(-1, len(data))))
+
+    predictions = np.array(predictions)
+
+    print(test_targets)
+    print(predictions)
+
+    return ((test_targets - predictions)**2).mean(axis=None)
+
 
 
 # Get data:
@@ -79,16 +96,32 @@ grouped_targets = semi_grouped[semi_grouped['type'] == 'target']
 del(grouped_data['time'])
 del(grouped_data['type'])
 
+del(grouped_data['open'])
+del(grouped_data['volumefrom'])
+
+
 # Cut trailing data (remember that we need to group by triplets):
 while len(grouped_data) & 3 > 0:
     grouped_data = grouped_data[:-1]
 
 
+# Scale data before grouping them:
+grouped_data = scale(grouped_data)
+
+grouped_data.head()
+
+
 X = [np.concatenate((grouped_data.iloc[a], grouped_data.iloc[a + 1], grouped_data.iloc[a + 2])) for a in range(0, len(grouped_data), 3)]
 y = grouped_targets['close'].values.tolist()
 
+X_last = np.array(X.pop())
+y_last = np.array(y.pop())
+
 X = np.array(X)
 y = np.array(y)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 
 features_number = len(X[0])
@@ -97,28 +130,48 @@ features_number = len(X[0])
 # Now let's use a neural network:
 def baseline_model():
     model = Sequential()
-    model.add(Dense(features_number * 2,   input_dim=features_number,   activation='relu'))
-    model.add(Dense(256,                                                activation='relu'))
-    model.add(Dense(512,                                                activation='relu'))
-    model.add(Dense(1024,                                               activation='relu'))
-    model.add(Dense(512,                                                activation='relu'))
-    model.add(Dense(256,                                                activation='relu'))
+    model.add(Dense(features_number,    input_dim=features_number,    activation='relu'))
+    model.add(Dense(1024,               kernel_initializer='normal',  activation='tanh'))
+    model.add(Dense(512,                kernel_initializer='normal',  activation='relu'))
     model.add(Dense(1))
 
-    model.compile(optimizer='adam', loss='mse')
+    model.compile(optimizer='adam', loss='mean_squared_error')
 
     return model
 
 
 # evaluate model with standardized dataset
 estimators = []
-scaler = MinMaxScaler()
 
 tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
 
-estimators.append(('standardizer', scaler))
-estimators.append(('regressor',    KerasRegressor(build_fn=baseline_model, epochs=100, batch_size=10)))
+estimators.append(('regressor', KerasRegressor(build_fn=baseline_model, epochs=200, batch_size=10, verbose=0)))
 
 pipeline = Pipeline(estimators)
 
-pipeline.fit(X, y, regressor__validation_split=0.3, regressor__callbacks=[tensorboard])
+pipeline.fit(X_train, y_train, regressor__callbacks=[tensorboard])
+
+mse = validate_model(pipeline, X_test, y_test)
+
+print('MSE: ', mse)
+
+# # Fix random seed for reproducibility
+# seed = 7
+# np.random.seed(seed)
+#
+# # Evaluate using 10-fold cross validation
+# kfold = KFold(n_splits=2, shuffle=True, random_state=seed)
+# results = cross_val_score(pipeline, X, y, cv=kfold)
+# print('Results: ', results.mean())
+
+predicted = pipeline.predict(X_last.reshape(-1, features_number))
+actual    = y_last
+
+difference = (1 - actual/predicted) * 100
+
+print('Predicted:', predicted)
+print('Actual:   ', actual)
+print('Error:    {}%'.format(round(difference, 2)))
+
+
+# pipeline.named_steps['regressor'].model.save('model.h5')
