dafd3d5082509e073aad8f2272dd993d98d50f2c
Nicola Orritos <nicola.orritos@gmail.com>
2019-02-18 00:01:46 +0100

    Added a predictor or the last triplet of prices data

---
+++ predict_next.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+from sklearn.ensemble import RandomForestRegressor
+import numpy as np
+import data_provider as dp
+import pandas as pd
+from joblib import load
+
+
+def prediction(data, clf):
+    # Scale them all:
+    data, min_values, max_values = dp.min_max_scale(data)
+    data = np.concatenate(data.iloc[:3].values.tolist())
+
+    features_number = len(data)
+
+    predicted = clf.predict(data.reshape(-1, features_number))[0]
+    predicted = dp.min_max_rescale(predicted, min_values['usd_close'], max_values['usd_close'])
+
+    return predicted
+
+
+resolution = 'day'
+
+data = dp.load_latest(resolution)
+
+clf = load('best_model_{0}.joblib'.format(resolution))
+
+y = prediction(data, clf)
+
+print('Predicted [resolution="{0}"]: '.format(resolution))
+print(y)
